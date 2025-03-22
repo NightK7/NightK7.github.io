88c54fcf8bdca7b87654462e879cffce254f2149
NightK7 <kevinlaveau7@gmail.com>
2025-03-22 15:44:39 +0100

    entretiendone

--- Roofer/js/scripts.js
+++ Roofer/js/scripts.js
@@ -261,3 +261,16 @@ function showSlides2(n) {
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 // POUR LA COMPARAISON DSE IMAGES
+const slider = document.querySelector(".image-comparison .slider");
+const beforeImage = document.querySelector(".image-comparison .before-image");
+const sliderLine = document.querySelector(".image-comparison .slider-line");
+const sliderIcon = document.querySelector(".image-comparison .slider-icon");
+
+slider.addEventListener("input", (e) => {
+    let sliderValue = e.target.value;
+
+    beforeImage.style.clipPath = `inset(0 ${100 - sliderValue}% 0 0)`;
+
+    sliderLine.style.left = sliderValue + "%";
+    sliderIcon.style.left = sliderValue + "%";
+});
