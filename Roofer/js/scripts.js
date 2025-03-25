aab7e9b894252ba16774d2be3459b428245579ce
NightK7 <kevinlaveau7@gmail.com>
2025-03-25 21:21:21 +0100

    upjs

--- Roofer/js/scripts.js
+++ Roofer/js/scripts.js
@@ -260,29 +260,31 @@ function showSlides(n) {
 }
 
 //slide2 avant-toit
-showSlides2(slideIndex);
+let slideIndex2 = 1;
+
+showSlides2(slideIndex2);
 
 function plusSlides2(n) {
-    showSlides2(slideIndex += n);
+    showSlides2(slideIndex2 += n);
 }
 
 function currentSlide2(n) {
-    showSlides2(slideIndex = n);
+    showSlides2(slideIndex2 = n);
 }
 function showSlides2(n) {
     let i;
     let slides = document.getElementsByClassName("mySlides2");
     let dots = document.getElementsByClassName("dot2");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
+    if (n > slides.length) { slideIndex2 = 1 }
+    if (n < 1) { slideIndex2 = slides.length }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+    slides[slideIndex2 - 1].style.display = "block";
+    dots[slideIndex2 - 1].className += " active";
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
